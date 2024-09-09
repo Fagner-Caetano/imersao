24bbfb664d7c30320498578175609088ea27fc70
Fagner Caetano <128565524+Fagner-Caetano@users.noreply.github.com>
2024-09-08 21:50:44 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Raysa Leal",
+        descricao : "Rayssa Leal é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspíra milhões de jovens.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate carisma sk8"
+    },
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é uma ginasta artística brasileira que fez história nos Jogos Olímpicos. Com sua força, determinação e talento, ela se tornou a maior medalhista olímpica do Brasil. Suas conquistas, tanto no individual geral quanto em provas específicas, inspiraram milhões de brasileiros e a posicionaram como um dos maiores nomes do esporte nacional.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    },
+
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos, trazendo orgulho para o país. Sua força e técnica a colocaram entre as melhores do mundo.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    }
+];
